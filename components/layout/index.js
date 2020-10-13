9a5cad498f69b5bcc0793890194940e488e08604
Berkay COBAN <bcobann35@gmail.com>
2020-10-13 14:54:46 +0300

    updated layout

--- components/layout/index.js
+++ components/layout/index.js
@@ -3,7 +3,11 @@ import React from 'react'
 import styles from './style.module.css'
 
 function Layout({ children }) {
-  return <div className={styles.layout}>{children}</div>
+  return (
+    <div className={styles.layout}>
+      <main>{children}</main>
+    </div>
+  )
 }
 
 export default Layout
