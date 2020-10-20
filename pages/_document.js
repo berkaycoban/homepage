ebbd8df80be8da9793d36bc7c10daf2c9fbc270d
Berkay COBAN <bcobann35@gmail.com>
2020-10-21 00:53:30 +0300

    :zap: added tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -50,6 +50,14 @@ class MyDocument extends Document {
           )}
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-TF6RDZ9"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
